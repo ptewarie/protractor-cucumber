e57fe2c8b714dc85437af1352d2c39528b5d4a55
praximus <pramod.tewarie@gmail.com>
2017-04-13 18:19:56 +0200

    test shell file

---
+++ report.bat
@@ -0,0 +1,2 @@
+echo "pramod"
+type ./report.json | ./node_modules/.bin/cucumber-junit > cucumber_report.xml
--- report.sh
+++
@@ -1,2 +0,0 @@
-echo "pramod"
-cat ./report.json | ./node_modules/.bin/cucumber-junit > cucumber_report.xml
