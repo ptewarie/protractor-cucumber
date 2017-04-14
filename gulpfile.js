97bee38675aba77b3597b7d9a0537d67e640eaa1
praximus <pramod.tewarie@gmail.com>
2017-04-15 01:29:14 +0200

    added reporting html

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,24 @@ var gulp = require('gulp'),
     protractor = require('gulp-protractor').protractor;
 
 
-    gulp.task('jenkins', function () {
+    gulp.task('shoppingBasket', function () {
         gulp.src(['/noNeedForThis'])
             .pipe(protractor({
                 configFile: 'demo.conf.js',
                 args: [
                     '--cucumberOpts.tags', '@shoppingBasket']
+            }))
+            .on('error', function (e) {
+                throw e;
+            });
+    });
+
+    gulp.task('E2E', function () {
+        gulp.src(['/noNeedForThis'])
+            .pipe(protractor({
+                configFile: 'demo.conf.js',
+                args: [
+                    '--cucumberOpts.tags', '@E2E']
             }))
             .on('error', function (e) {
                 throw e;
