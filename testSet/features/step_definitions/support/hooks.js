1a3e8df17673bb40f380341273ae9fda9933ff27
praximus <pramod.tewarie@gmail.com>
2017-04-15 01:05:42 +0200

    added screenshot in reporter

--- testSet/features/step_definitions/support/hooks.js
+++ testSet/features/step_definitions/support/hooks.js
@@ -7,28 +7,42 @@ var reporter = require('cucumber-html-reporter');
 
 
 
-
-
-
 module.exports = function JsonOutputHook() {
 
 
+    this.After(function(scenario, callback) {
+        browser.takeScreenshot().then(function(base64png) {
+            var decodedImage = new Buffer(base64png, 'base64');
+            scenario.attach(decodedImage, 'image/png', function (error) {
+                callback(error);
+            });
+        }, function(err) {
+            callback(err);
+        });
+    });
+
     this.registerHandler('AfterFeatures', function(features, callback) {
         var options = {
-            theme: 'bootstrap',
+            theme: 'foundation',
+            // theme: 'bootstrap',
+            // theme: 'simple',
             jsonFile: 'reports/cucumber-test-results.json',
             output: 'reports/cucumber_report.html',
             reportSuiteAsScenarios: true,
             launchReport: true,
             metadata: {
-                "App Version": "0.3.2",
-                "Test Environment": "STAGING",
-                "Browser": "Chrome  54.0.2840.98",
-                "Platform": "Windows 10",
+                "Title": "0.3.2",
+                "Author": "Pramod",
+                "Browser": "Chrome  56",
+                "Platform": "Windows 7",
                 "Parallel": "Scenarios",
                 "Executed": "Remote"
             }
+
         };
+        browser.driver.takeScreenshot().then(function (buffer) {
+            return scenario.attach(new Buffer(buffer, 'base64'), 'image/png');
+        });
 
         reporter.generate(options);
         callback();
