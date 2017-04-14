2f73bcf7d4cafa1e16c4968b779a0df7d02a184d
praximus <pramod.tewarie@gmail.com>
2017-04-14 12:44:48 +0200

    cleaned config file

--- demo.conf.js
+++ demo.conf.js
@@ -1,10 +1,9 @@
 exports.config = {
-//
+
     seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
     getPageTimeout: 60000,
     allScriptsTimeout: 500000,
     framework: 'custom',
-    // path relative to the current config file
     frameworkPath: require.resolve('protractor-cucumber-framework'),
     capabilities: {
         'browserName': 'chrome'
@@ -12,7 +11,6 @@ exports.config = {
         // 'version': '11'
     },
 
-    // Spec patterns are relative to this directory.
     specs: [
         './testSet/features/*.feature'
     ],
